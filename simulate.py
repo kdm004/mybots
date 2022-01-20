2c3790e460556dcb937cb847070691250e37a7a4
kdm004 <kevindmotia@gmail.com>
2022-01-20 12:08:53 -0500

    Finished SIMULATION STEP 18.

--- simulate.py
+++ simulate.py
@@ -1,5 +1,12 @@
 import pybullet as p
+import time
 physicsClient = p.connect(p.GUI)
+for i in range (1000):
+    p.stepSimulation()
+    time.sleep(1/80)
+    print('For loop variable is',i)
+
+
 p.disconnect()
 
 
