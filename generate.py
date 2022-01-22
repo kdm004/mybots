23db56f41cc4c7c7b62e6d37835661c4f4d89f9a
kdm004 <kevindmotia@gmail.com>
2022-01-22 17:36:18 -0500

    Edited generate variables

--- generate.py
+++ generate.py
@@ -1,8 +1,11 @@
 import pyrosim.pyrosim as pyrosim
+length = 1
+width = 1
+height = 1
 
 pyrosim.Start_SDF("box.sdf")
 
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[length,width,height])
 
 pyrosim.End()
 
