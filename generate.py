557187cb7ccce198febe057b6e309e5954767cc3
kdm004 <kevindmotia@gmail.com>
2022-01-22 17:08:44 -0500

    Manylinks

--- generate.py
+++ generate.py
@@ -1,9 +1,26 @@
 import pyrosim.pyrosim as pyrosim
+pyrosim.Start_SDF("boxes.sdf")
 
-pyrosim.Start_SDF("box.sdf")
+length = 1/.9
+width = 1/.9
+height = 1/.9
+x = 0
+y = 0
+z = .5
 
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+for i in range(6):
+    x=i
+    for j in range(6):
+        y=j
+        new_length = length
+        new_width = width
+        new_height = height
+        for k in range(10):
+            new_length *= .9
+            new_width *= .9
+            new_height *= .9
+            pyrosim.Send_Cube(name="Box", pos=[x,y,z+k] , size=[new_length,new_width,new_height])
 
-pyrosim.End()
+pyrosim.End() #Flat base, correctly positioned, and correct tower.
 
 
