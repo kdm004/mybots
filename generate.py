559368ac7a3e330e16a786f0a3a58246c103b067
kdm004 <kevindmotia@gmail.com>
2022-01-23 13:44:42 -0500

    Manylinks tower attempt2 tabs

--- generate.py
+++ generate.py
@@ -11,12 +11,16 @@ z = .5 * height
 for i in range(10):
     x += i
     for j in range (10):
-    y += 1
-    for k in range(10):
-    new_length *= .9
-    new_width *= .9
-    new_height *= .9
-    pyrosim.Send_Cube(name="Box2", pos=[a,b,c] , size=[length,width,height])
+        y += 1
+        new_length *= .9
+        new_width *= .9
+        new_height *= .9
+
+        for k in range(10):
+            new_length *= .9
+            new_width *= .9
+            new_height *= .9
+            pyrosim.Send_Cube(name="Box2", pos=[a,b,c] , size=[length,width,height])
 
 pyrosim.End()
 
