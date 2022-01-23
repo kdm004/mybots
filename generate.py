8b4167230d901b23efcb35dad02ff528c983dc01
kdm004 <kevindmotia@gmail.com>
2022-01-23 13:41:57 -0500

    Manylinks tower attempt1

--- generate.py
+++ generate.py
@@ -1,4 +1,6 @@
 import pyrosim.pyrosim as pyrosim
+pyrosim.Start_SDF("boxes.sdf")
+
 length = 1
 width = 1
 height = 1
@@ -6,13 +8,15 @@ x = 0
 y = 0
 z = .5 * height
 
-a = 1
-b = 0
-c = (.5 * height) + height  #Multiplied height by 2 to increase box starting height by 1 box
-pyrosim.Start_SDF("boxes.sdf")
-
-pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
-pyrosim.Send_Cube(name="Box2", pos=[a,b,c] , size=[length,width,height])
+for i in range(10):
+    x += i
+    for j in range (10):
+    y += 1
+    for k in range(10):
+    new_length *= .9
+    new_width *= .9
+    new_height *= .9
+    pyrosim.Send_Cube(name="Box2", pos=[a,b,c] , size=[length,width,height])
 
 pyrosim.End()
 
