9b39ef48e7626d287e8112bcb6ba6b1395627eb1
Jay Motia <jmotia@dynapower.com>
2022-01-23 17:17:56 -0500

    Towers flush but total out of position

--- generate.py
+++ generate.py
@@ -8,19 +8,23 @@ x = 0
 y = 0
 z = .5
 
+
 for i in range(6):
-    x+=i
+    x+= .9*i
     for j in range(6):
-        y+=1
+        y=.9
         new_length = length
         new_width = width
         new_height = height
+        
         for k in range(10):
             new_length *= .9
             new_width *=.9
             new_height *= .9
-            pyrosim.Send_Cube(name="Box", pos=[x,y,z+k] , size=[length,width,height])
+            pyrosim.Send_Cube(name="Box", pos=[x,y,z+k] , size=[new_length,new_width,new_height])
 
+    x=0
+    y=0
 pyrosim.End()
 
 
