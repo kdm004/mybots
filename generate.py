e0c15563c11a9f33f6f84714bce0c845ff353300
kdm004 <kevindmotia@gmail.com>
2022-01-23 13:07:26 -0500

    Againx3 trying to add 1boxheight

--- generate.py
+++ generate.py
@@ -8,7 +8,7 @@ z = .5 * height
 
 a = 1
 b = 0
-c = .5 * height + 1  #Multiplied height by 2 to increase box starting height by 1 box
+c = (.5 * height) + height  #Multiplied height by 2 to increase box starting height by 1 box
 pyrosim.Start_SDF("boxes.sdf")
 
 pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
