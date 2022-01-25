390ddbbdd2851dcd8f557a74ef63c182c3045af8
Jay Motia <jmotia@dynapower.com>
2022-01-24 21:01:12 -0500

    Tower city done.

--- generate.py
+++ generate.py
@@ -1,30 +1,26 @@
 import pyrosim.pyrosim as pyrosim
 pyrosim.Start_SDF("boxes.sdf")
 
-length = 1
-width = 1
-height = 1
+length = 1/.9
+width = 1/.9
+height = 1/.9
 x = 0
 y = 0
 z = .5
 
-
 for i in range(6):
-    x+= .9*i
+    x=i
     for j in range(6):
-        y=.9
+        y=j
         new_length = length
         new_width = width
         new_height = height
-        
         for k in range(10):
             new_length *= .9
-            new_width *=.9
+            new_width *= .9
             new_height *= .9
             pyrosim.Send_Cube(name="Box", pos=[x,y,z+k] , size=[new_length,new_width,new_height])
 
-    x=0
-    y=0
-pyrosim.End()
+pyrosim.End() #Flat base, correctly positioned, and correct tower.
 
 
