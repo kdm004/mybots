194fe8af6aaf2ab856a1523692bfe52fb0e745cf
Kevin Motia <kevinmotia@Kevins-MacBook-Pro.local>
2022-01-28 18:39:16 -0500

    The white and green boxes spawn at origin

--- generate.py
+++ generate.py
@@ -1,6 +1,16 @@
 import pyrosim.pyrosim as pyrosim
-pyrosim.Start_SDF("world.sdf")
 
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+    pyrosim.End()
+
+def Create_Robot():               
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Torso", pos=[x,y,z] , size=[length,width,height])
+    pyrosim.End()
+
+#Cube size (length, width, height) and position (x,y,z)
 length = 1
 width = 1
 height = 1
@@ -8,8 +18,5 @@ x = 0
 y = 0
 z = .5
 
-pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
-
-pyrosim.End()
-
-
+Create_World()
+Create_Robot()
