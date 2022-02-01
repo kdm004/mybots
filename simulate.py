c683319f0c44c03e36dd89e7e0e3f937491bb544
Kevin Motia <kevinmotia@Kevins-MacBook-Pro.local>
2022-01-31 20:01:04 -0500

    Created vector to store touch sensor values

--- simulate.py
+++ simulate.py
@@ -1,3 +1,4 @@
+import numpy as numpy
 import pyrosim.pyrosim as pyrosim
 import pybullet_data
 import pybullet as p
@@ -9,12 +10,14 @@ planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate(robotId)
+backLegSensorValues = numpy.zeros(1000)
+
 for i in range (1000):
     p.stepSimulation()
-    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
-    print(backLegTouch)
+    backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     time.sleep(1/80)
     print('For loop variable is',i)
+    print(backLegSensorValues)
 
 
 p.disconnect()
