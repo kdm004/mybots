e032923b4d14bcaa01714ad7e01823ac623bab50
Kevin Motia <kevinmotia@Kevins-MacBook-Pro.local>
2022-01-31 19:40:53 -0500

    First touch sensor back leg

--- simulate.py
+++ simulate.py
@@ -1,3 +1,4 @@
+import pyrosim.pyrosim as pyrosim
 import pybullet_data
 import pybullet as p
 import time
@@ -7,8 +8,11 @@ p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
+pyrosim.Prepare_To_Simulate(robotId)
 for i in range (1000):
     p.stepSimulation()
+    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    print(backLegTouch)
     time.sleep(1/80)
     print('For loop variable is',i)
 
