e212c4f82826969a7f86bb4dbb6c0e3348580e4d
Kevin Motia <kevinmotia@Kevins-MacBook-Pro.local>
2022-01-31 22:21:58 -0500

    Show a graph for the first time

---
+++ analyze.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot
+import numpy as numpy
+backLegSensorValues = numpy.load('data/backLegSensorValues.npy')
+matplotlib.pyplot.plot(backLegSensorValues)
+matplotlib.pyplot.show()
+
+
+
+
+
+
+
