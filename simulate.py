fcf2200f4cfee67a1b7b85adbd10d8fc8f5a56cb
Kevin Motia <kevinmotia@Kevins-MacBook-Pro.local>
2022-02-03 12:06:06 -0500

    Added targetAngles stuff for the first time

--- simulate.py
+++ simulate.py
@@ -1,3 +1,4 @@
+import random as random
 import numpy as numpy
 import pyrosim.pyrosim as pyrosim
 import pybullet_data
@@ -12,31 +13,36 @@ p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate(robotId)
 
 # Vectors for sensor values
-backLegSensorValues = numpy.zeros(100)
-frontLegSensorValues = numpy.zeros(100)
+backLegSensorValues = numpy.zeros(1000)
+frontLegSensorValues = numpy.zeros(1000)
 
-for i in range (100):
+targetAngles = numpy.linspace(0,2*(numpy.pi), 1000)
+    
+
+for i in range (1000):
     p.stepSimulation()
 
     # Sensor values
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
 
+
     pyrosim.Set_Motor_For_Joint(
     bodyIndex = robotId,
     jointName = "Torso_BackLeg",
     controlMode = p.POSITION_CONTROL,
-    targetPosition = -(numpy.pi)/4,
-    maxForce = 500)  
+    targetPosition = random.random(),
+    maxForce = 10)  
 
     pyrosim.Set_Motor_For_Joint(
     bodyIndex = robotId,
     jointName = "Torso_FrontLeg",
     controlMode = p.POSITION_CONTROL,
-    targetPosition = (numpy.pi)/4,
-    maxForce = 500)  
+    targetPosition = random.random(),
+    maxForce = 10)
+  
 
-    time.sleep(1/5)
+    time.sleep(1/(800000))
     print('For loop variable is',i)
     print(backLegSensorValues)
     print(frontLegSensorValues)
@@ -44,4 +50,7 @@ for i in range (100):
 # Save Sensor Values here 
 numpy.save('data/backLegSensorValues.npy', backLegSensorValues)
 numpy.save('data/frontLegSensorValues.npy', frontLegSensorValues)
+# Save Target Angles here
+numpy.save('data/targetAngles.npy', targetAngles)
+
 p.disconnect()
