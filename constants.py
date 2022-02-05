234eeb2c10cec206f0b18c1cf6cebbecaa41e238
Kevin Motia <kevinmotia@Kevins-MacBook-Pro.local>
2022-02-05 12:28:27 -0500

    Added some constants to constants.py

---
+++ constants.py
@@ -0,0 +1,22 @@
+import numpy as numpy
+#------------------------------------------
+
+
+loopLength = 1000
+
+BackLeg_amplitude = numpy.pi/4
+BackLeg_frequency = 10
+BackLeg_phaseOffset = numpy.pi/2
+
+FrontLeg_amplitude = numpy.pi/4
+FrontLeg_frequency = 20
+FrontLeg_phaseOffset = 0
+
+
+
+
+
+
+
+
+
