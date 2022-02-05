696d2af0c73c804be1692855e9b6fa33e4f4345a
Kevin Motia <kevinmotia@Kevins-MacBook-Pro.local>
2022-02-05 17:44:19 -0500

    Created simulation.py with robot and world inside

---
+++ simulation.py
@@ -0,0 +1,8 @@
+# This is the new simulation file that was created in the refactoring branch
+from world import WORLD
+from robot import ROBOT
+
+class SIMULATION:
+    def __init__(self):
+        self.world = WORLD()
+        self.robot = ROBOT()
