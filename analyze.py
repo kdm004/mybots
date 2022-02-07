619402abcdc780a448b78d5c4b0c95b29980568f
Kevin Motia <kevinmotia@Kevins-MacBook-Pro.local>
2022-02-07 15:41:09 -0500

    Not sure if anything is changed here but adding it anyway

--- analyze.py
+++ analyze.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot
 import numpy as numpy
-#import matplotlib.pylab as plt
+import matplotlib.pylab as plt
 
 backLegSensorValues = numpy.load('data/backLegSensorValues.npy')
 frontLegSensorValues = numpy.load('data/frontLegSensorValues.npy')
@@ -9,18 +9,21 @@ frontLegSensorValues = numpy.load('data/frontLegSensorValues.npy')
 #matplotlib.pyplot.plot(frontLegSensorValues, label = 'Front Leg')
 #matplotlib.pyplot.legend()
 #matplotlib.pyplot.show()
-
-motorCommand = numpy.load('data/motorCommand.npy')
-#plt.plot(motorCommand, numpy.sin(motorCommand))
+#--------------------------------------
+BackLeg_motorCommand = numpy.load('data/BackLeg_motorCommand.npy')
+#plt.plot(BackLeg_motorCommand, numpy.sin(BackLeg_motorCommand))
 #plt.xlabel('Angle [rad]')
-#plt.ylabel('sin(motorCommand)')
+#plt.ylabel('sin(BackLeg_motorCommand)')
 #plt.axis('tight')
 #plt.show()
-motorPlot = matplotlib.pyplot.plot(numpy.sin(motorCommand))
+
+
+motorPlot = matplotlib.pyplot.plot(numpy.sin(BackLeg_motorCommand))
 matplotlib.pyplot.show()
+#--------------------------------------
+#matplotlib.pyplot.plot(BackLeg_motorCommand)
+#matplotlib.pyplot.show()
 #
-
-
 
 
 
