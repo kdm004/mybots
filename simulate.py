6e69f2ea083c84ab447b49b0241778fa465d085c
Kevin Motia <kevinmotia@Kevins-MacBook-Pro.local>
2022-02-13 14:15:42 -0500

    Updating all. Finished step 29.

--- simulate.py
+++ simulate.py
@@ -13,6 +13,7 @@ simulation = SIMULATION()
 #physicsClient = p.connect(p.GUI)
 #p.setAdditionalSearchPath(pybullet_data.getDataPath())
 #p.setGravity(0,0,-9.8)
+
 #planeId = p.loadURDF("plane.urdf")
 #robotId = p.loadURDF("body.urdf")
 #p.loadSDF("world.sdf")
--- simulation.py
+++ simulation.py
@@ -1,9 +1,15 @@
 # This is the new simulation file that was created in the refactoring branch
 from world import WORLD
 from robot import ROBOT
+import pybullet as p
+import pybullet_data
 
 class SIMULATION:
     def __init__(self):
-        pass
+        self.physicsClient = p.connect(p.GUI)
+        p.setAdditionalSearchPath(pybullet_data.getDataPath())
+        p.setGravity(0,0,-9.8)
         self.world = WORLD()
         self.robot = ROBOT()
+        
+     
