79fcfe21b15ddc336432c175b68d49a409d7318a
Kevin Motia <kevinmotia@Kevins-MacBook-Pro.local>
2022-02-13 14:22:40 -0500

    Updating all. Finished step 32.

--- robot.py
+++ robot.py
@@ -1,4 +1,12 @@
+import pyrosim.pyrosim as pyrosim
+import pybullet as p
+
+
+
 class ROBOT:
     def __init__(self):
         self.sensors = {}
         self.motors = {}
+
+        self.robotId = p.loadURDF("body.urdf")
+        pyrosim.Prepare_To_Simulate(robotId)
--- simulate.py
+++ simulate.py
@@ -10,13 +10,11 @@ from simulation import SIMULATION
 simulation = SIMULATION()
 
 #------------------------------------------------------------
-#physicsClient = p.connect(p.GUI)
-#p.setAdditionalSearchPath(pybullet_data.getDataPath())
-#p.setGravity(0,0,-9.8)
 
-#planeId = p.loadURDF("plane.urdf")
+
 #robotId = p.loadURDF("body.urdf")
-#p.loadSDF("world.sdf")
+
+
 #pyrosim.Prepare_To_Simulate(robotId)
 
 
