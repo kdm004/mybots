59dc3144ebe9f4b3f275501b4908ca1b941615cd
Kevin Motia <kevinmotia@Kevins-MacBook-Pro.local>
2022-02-13 14:17:00 -0500

    Updating all. Finished step 29 for real

--- world.py
+++ world.py
@@ -4,7 +4,7 @@ import pybullet_data
 
 class WORLD:
     def __init__(self):
-        planeId = p.loadURDF("plane.urdf")
+        self.planeId = p.loadURDF("plane.urdf")
         p.loadSDF("world.sdf")
 
 
