1d34356736ac8a9d573d4f3daf4ce05baba3a5cd
Kevin Motia <kevinmotia@Kevins-MacBook-Pro.local>
2022-02-16 18:22:10 -0500

    Step 98. Moving robot

--- simulate.py
+++ simulate.py
@@ -9,25 +9,6 @@ from simulation import SIMULATION
 
 simulation = SIMULATION()
 simulation.Run()
-#------------------------------------------------------------
-
-
-
-
-#-------------------------------------------------------------
-## Vectors for sensor values  (moving sensor values to sensor.py) #####
-#backLegSensorValues = numpy.zeros(c.loopLength)
-#frontLegSensorValues = numpy.zeros(c.loopLength)
-
-## Vector for target angles (Now found in constants.py as of step 82 refactoring)
-#targetAngles = numpy.linspace(-numpy.pi,numpy.pi, c.loopLength)
-
-## Defining leg motor commands (Now found in motor.py)
-#BackLeg_motorCommand = c.BackLeg_amplitude * numpy.sin(c.BackLeg_frequency * targetAngles + c.BackLeg_phaseOffset)
-#FrontLeg_motorCommand = c.FrontLeg_amplitude * numpy.sin(c.FrontLeg_frequency * targetAngles + c.FrontLeg_phaseOffset)
-#--------------------------------------------------------------
-
-
 
 #numpy.save('data/motorCommand.npy', motorCommand)
 #exit()
