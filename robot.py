07c167987541324826f798f879f926250c56128b
Kevin Motia <kevinmotia@Kevins-MacBook-Pro.local>
2022-02-15 23:44:36 -0500

    Showing vectors. Finished step 69

--- robot.py
+++ robot.py
@@ -7,19 +7,22 @@ import numpy as numpy
 
 class ROBOT:
     def __init__(self):
+        self.sensors = {}
         self.motors = {}
+        self.values = {}     # are you sure? why not just put the numpy.zeros thing as the first line in Sense() down here?
+
         self.robotId = p.loadURDF("body.urdf")
         pyrosim.Prepare_To_Simulate(self.robotId)
         self.Prepare_To_Sense()
 
     def Prepare_To_Sense(self):
-        self.sensors = {}
-        self.values = {} # are you sure? why not just put the numpy.zeros thing as the first line in Sense() down here?
         for linkName in pyrosim.linkNamesToIndices:
             self.sensors[linkName] = SENSOR(linkName)
 
     def Sense(self,t):
         for key in self.sensors:
+            print(self.sensors)
+            print('key is ', key)
             self.values[t] =self.sensors[key].Get_Value(t)
  
 
--- sensor.py
+++ sensor.py
@@ -9,8 +9,7 @@ class SENSOR:
 
     def Get_Value(self,t):
         self.values[t] = pyrosim.Get_Touch_Sensor_Value_For_Link(self.linkName) #took out quotes from "self.linkName"       
-        #return self.values     
-        print(self.values[t])
+        return self.values     
 
 
 
