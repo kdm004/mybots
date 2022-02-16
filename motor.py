937492c5e857621b4181f8fced58e97b87f7e901
Kevin Motia <kevinmotia@Kevins-MacBook-Pro.local>
2022-02-16 15:29:13 -0500

    Step 77 NoneType object is not iterable

--- motor.py
+++ motor.py
@@ -1,3 +1,8 @@
+import constants as c
+import numpy as numpy
+import pyrosim.pyrosim as pyrosim
+
+
 class MOTOR:
     def __init__(self, jointName):
         self.jointName = jointName
@@ -5,3 +10,5 @@ class MOTOR:
 
     def Prepare_To_Act(self):
         pass
+        #BackLeg_motorCommand = c.BackLeg_amplitude * numpy.sin(c.BackLeg_frequency * targetAngles + c.BackLeg_phaseOffset)
+        #FrontLeg_motorCommand = c.FrontLeg_amplitude * numpy.sin(c.FrontLeg_frequency * targetAngles + c.FrontLeg_phaseOffset)        
