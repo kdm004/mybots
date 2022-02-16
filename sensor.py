62abb5bc388c4909eef91a33e58f6f66072d275f
Kevin Motia <kevinmotia@Kevins-MacBook-Pro.local>
2022-02-15 20:22:44 -0500

    Updating all. Finished step 62 refactoring

--- sensor.py
+++ sensor.py
@@ -4,6 +4,14 @@ import numpy as numpy
 
 class SENSOR:
     def __init__(self,linkName):
-        self.values = numpy.zeros(c.loopLength)
-        
+        self.values = numpy.zeros(c.loopLength)   
         self.linkName = linkName
+
+    def Get_Value():
+        backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")        
+     
+
+
+
+
+
