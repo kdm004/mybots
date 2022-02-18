d0c6433e47940c68096de8cd828d386b9be29414
Kevin Motia <kevinmotia@Kevins-MacBook-Pro.local>
2022-02-18 17:27:23 -0500

    Finished step 42 neurons

--- pyrosim/neuron.py
+++ pyrosim/neuron.py
@@ -123,4 +123,4 @@ class NEURON:
         self.value = math.tanh(self.value)
 
     def Update_Sensor_Neuron(self):
-        self.Set_Value(0)
+        self.Set_Value(pyrosim.Get_Touch_Sensor_Value_For_Link(self.Get_Link_Name()))
