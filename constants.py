19df01dcb2ffdf0911f7f0c0fbba02a19b520824
Kevin Motia <kevinmotia@Kevins-MacBook-Pro.local>
2022-02-19 18:00:13 -0500

    messed around with gravity and amplitude to get a better video for refactoring

--- constants.py
+++ constants.py
@@ -4,16 +4,16 @@ import numpy as numpy
 
 loopLength = 1000
 
-amplitude = numpy.pi/4
+amplitude = numpy.pi/20 # change back to numpy.pi/4
 frequency = 10
 phaseOffset = numpy.pi/2
 legMaxForce = 20
 
 targetAngles = numpy.linspace(-numpy.pi,numpy.pi, loopLength)
 
-sleepRate = 1/260
+sleepRate = 1/800
 
-gravityConstant = -9.8
+gravityConstant = -.5
 
 
 
