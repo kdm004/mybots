0acb3efcfc87afabb8363b249129e588df56f42e
Kevin Motia <kevinmotia@Kevins-MacBook-Pro.local>
2022-02-23 15:30:33 -0500

    step 18 hillclimber

--- solution.py
+++ solution.py
@@ -3,6 +3,4 @@ import numpy
 class SOLUTION:
     def __init__(self):
         self.weights = numpy.random.rand(3,2)
-        print('before',self.weights)
         self.weights = self.weights * 2 - 1
-        print('after',self.weights)
