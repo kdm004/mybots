0a783c9373a80b8830dd0ca928c3b9d1059d12b7
Kevin Motia <kevinmotia@Kevins-MacBook-Pro.local>
2022-02-23 17:54:02 -0500

    step 33 not working

--- solution.py
+++ solution.py
@@ -1,5 +1,6 @@
 import numpy
 import pyrosim.pyrosim as pyrosim
+import os
 #--------------------------------------------
 #Cube size (length, width, height) and position (x,y,z)
 length = 1
@@ -31,14 +32,14 @@ class SOLUTION:
         self.weights = self.weights * 2 - 1
 
     def Evaluate(self):
-        pass
+        os.system("python3 simulate.py")
 
-    def Create_World():
+    def Create_World(self):
         pyrosim.Start_SDF("world.sdf")
         pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
         pyrosim.End()
 
-    def Generate_Body(): 
+    def Generate_Body(self): 
         pyrosim.Start_URDF("body.urdf")
         pyrosim.Send_Cube(name="Torso", pos=[x0,y0,z0] , size=[length,width,height])
         pyrosim.Send_Joint( name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [1,0,1])
@@ -49,20 +50,18 @@ class SOLUTION:
 
         pyrosim.End()
 
-    def Generate_Brain(): 
+    def Generate_Brain(self): 
         pyrosim.Start_NeuralNetwork("brain.nndf")
+
+        pyrosim.Send_Sensor_Neuron(name = 0 , linkName = "Torso")
+        pyrosim.Send_Sensor_Neuron(name = 1 , linkName = "BackLeg")
+        pyrosim.Send_Sensor_Neuron(name = 2 , linkName = "FrontLeg")
+        pyrosim.Send_Motor_Neuron( name = 3 , jointName = "Torso_BackLeg")
+        pyrosim.Send_Motor_Neuron( name = 4 , jointName = "Torso_FrontLeg")
    
-        for i in range(3):
-            pyrosim.Send_Sensor_Neuron(name = 0 , linkName = "Torso")
-            pyrosim.Send_Sensor_Neuron(name = 1 , linkName = "BackLeg")
-            pyrosim.Send_Sensor_Neuron(name = 2 , linkName = "FrontLeg")
-        
-            for j in range(3):
-                pyrosim.Send_Motor_Neuron( name = 3 , jointName = "Torso_BackLeg")
-                pyrosim.Send_Motor_Neuron( name = 4 , jointName = "Torso_FrontLeg")
+        for currentRow in range(3):
+ 
+            for currentColumn in range(2):
+                pyrosim.Send_Synapse( sourceNeuronName = currentRow , targetNeuronName = currentColumn+3 , weight = self.weights[currentRow][currentColumn] ) #step 7 randomsearch
 
-                pyrosim.Send_Synapse( sourceNeuronName = i , targetNeuronName = j , weight = 1.0 ) #step 7 randomsearch
-
-
-
-                
+        pyrosim.End()
