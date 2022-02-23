c456c5fd8a9d0f4beada5d5ea12814a0c266ec9d
Kevin Motia <kevinmotia@Kevins-MacBook-Pro.local>
2022-02-23 17:54:02 -0500

    step 33 not working

--- generate.py
+++ generate.py
@@ -31,24 +31,22 @@ def Generate_Body():
 
 def Generate_Brain(): 
     pyrosim.Start_NeuralNetwork("brain.nndf")
-   
+    pyrosim.Send_Sensor_Neuron(name = 0 , linkName = "Torso")
+    pyrosim.Send_Sensor_Neuron(name = 1 , linkName = "BackLeg")
+    pyrosim.Send_Sensor_Neuron(name = 2 , linkName = "FrontLeg")
+    pyrosim.Send_Motor_Neuron( name = 3 , jointName = "Torso_BackLeg")
+    pyrosim.Send_Motor_Neuron( name = 4 , jointName = "Torso_FrontLeg")
     for i in range(3):
-        pyrosim.Send_Sensor_Neuron(name = 0 , linkName = "Torso")
-        pyrosim.Send_Sensor_Neuron(name = 1 , linkName = "BackLeg")
-        pyrosim.Send_Sensor_Neuron(name = 2 , linkName = "FrontLeg")
-        
-        for j in range(3):
-            pyrosim.Send_Motor_Neuron( name = 3 , jointName = "Torso_BackLeg")
-            pyrosim.Send_Motor_Neuron( name = 4 , jointName = "Torso_FrontLeg")
 
-            pyrosim.Send_Synapse( sourceNeuronName = i , targetNeuronName = j , weight = 1.0 ) #step 7 randomsearch
+        for j in range(2):
+            pyrosim.Send_Synapse( sourceNeuronName = i , targetNeuronName = j , weight = random.uniform(-1.0,1.0) ) #step 7 randomsearch
 
 
 
-    pyrosim.Send_Synapse( sourceNeuronName = 0 , targetNeuronName = 3 , weight = random.uniform(-1.0,1.0) )
-    pyrosim.Send_Synapse( sourceNeuronName = 1 , targetNeuronName = 3 , weight = random.uniform(-1.0,1.0) )
-    pyrosim.Send_Synapse( sourceNeuronName = 0 , targetNeuronName = 4 , weight = random.uniform(-1.0,1.0) )
-    pyrosim.Send_Synapse( sourceNeuronName = 1 , targetNeuronName = 4 , weight = random.uniform(-1.0,1.0) )
+    #pyrosim.Send_Synapse( sourceNeuronName = 0 , targetNeuronName = 3 , weight = random.uniform(-1.0,1.0) )
+    #pyrosim.Send_Synapse( sourceNeuronName = 1 , targetNeuronName = 3 , weight = random.uniform(-1.0,1.0) )
+    #pyrosim.Send_Synapse( sourceNeuronName = 0 , targetNeuronName = 4 , weight = random.uniform(-1.0,1.0) )
+    #pyrosim.Send_Synapse( sourceNeuronName = 1 , targetNeuronName = 4 , weight = random.uniform(-1.0,1.0) )
 
     pyrosim.End()
 #-------------------------------------------------------------
