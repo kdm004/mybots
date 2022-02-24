76e4fa38fd78283b39a45c1339fd78b47aea4842
Kevin Motia <kevinmotia@Kevins-MacBook-Pro.local>
2022-02-24 10:55:38 -0500

    step 33 hillclimber. Working. Fixed generate.py Send_Synapse =j needed to be = j+3

--- search.py
+++ search.py
@@ -6,7 +6,7 @@ hc = HILL_CLIMBER()
 
 hc.Evolve()
 
-for i in range(2): #this is number of simulation windows that will pop up. 3 are currently popping
+#for i in range(2): #this is number of simulation windows that will pop up. 3 are currently popping
     # up because there's another "simulate.py" call in SOLUTION's Evaluate(self)
-    os.system("python3 generate.py")
-    os.system("python3 simulate.py")
+#    os.system("python3 generate.py")
+#    os.system("python3 simulate.py")
