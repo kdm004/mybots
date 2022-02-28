fba06139cfd1c7f4c836a17c0bc4c473b5c437cc
Kevin Motia <kevinmotia@Kevins-MacBook-Pro.local>
2022-02-28 14:22:34 -0500

    Housekeeping

--- solution.py
+++ solution.py
@@ -32,11 +32,11 @@ class SOLUTION:
         self.weights = numpy.random.rand(3,2)
         self.weights = self.weights * 2 - 1
 
-    def Evaluate(self,mode):
+    def Evaluate(self,directOrGUI):
         self.Create_World()
         self.Generate_Body()
         self.Generate_Brain()
-        os.system("python3 simulate.py " + mode) # changed from "DIRECT" to mode
+        os.system("python3 simulate.py " + directOrGUI) # changed from "DIRECT" to directOrGUI
         fitnessFile = open("fitness.txt","r")
         self.fitness = float(fitnessFile.read()) #Used fitnessFile, they normally use f
         fitnessFile.close()
