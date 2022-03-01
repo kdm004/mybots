cd5ef52f7f6fa8cf4b526328e1881ccd137184ad
Kevin Motia <kevinmotia@Kevins-MacBook-Pro.local>
2022-03-01 13:17:55 -0500

    step 45 parallelHC...argv error magically resolved

--- solution.py
+++ solution.py
@@ -37,7 +37,7 @@ class SOLUTION:
         self.Create_World()
         self.Generate_Body()
         self.Generate_Brain()
-        os.system("python3 simulate.py " + directOrGUI + " &") # changed from "DIRECT" to directOrGUI
+        os.system("python3 simulate.py " + directOrGUI + " " + str(self.myID) + " &") # changed from "DIRECT" to directOrGUI
         fitnessFile = open("fitness.txt","r")
         self.fitness = float(fitnessFile.read()) #Used fitnessFile, they normally use f
         fitnessFile.close()
