a97fe11f1168648d0416a4af28593ec2e90c3615
Kevin Motia <kevinmotia@Kevins-MacBook-Pro.local>
2022-03-01 18:25:25 -0500

    step 83 parallelHC

--- parallelHillClimber.py
+++ parallelHillClimber.py
@@ -1,9 +1,13 @@
 from solution import SOLUTION
 import constants as c
 import copy
+import os
 #------------------------------------
 class PARALLEL_HILL_CLIMBER:
     def __init__(self):
+
+        os.system("rm brain*.nndf") # step 82 parallelHC
+        os.system("rm fitness*.txt") # step 83 parallelHC
         self.parents = {}
         self.children = {}
         self.nextAvailableID = 0
@@ -15,23 +19,24 @@ class PARALLEL_HILL_CLIMBER:
     def Evolve(self): 
         for i in range(len(self.parents)):
             self.parents[i].Start_Simulation("DIRECT") #step 69 parallelHC -- GUI -> DIRECT
-        for i in range(len(self.parents)):
+        for i in range(len(self.parents)):            #step 72 parallelHC... uncomment to activate Parallelism, comment to deactivate Parallelism
             self.parents[i].Wait_For_Simulation_To_End()
-        #for currentGeneration in range(c.numberOfGenerations): # to get just the first gen, set c.numberOfGenerations = 1
-            #self.Evolve_For_One_Generation()
+        for currentGeneration in range(c.numberOfGenerations): # to get just the first gen, set c.numberOfGenerations = 1
+            self.Evolve_For_One_Generation()
 
     def Evolve_For_One_Generation(self):
         self.Spawn()
-        self.Mutate()
-        self.child.Evaluate("DIRECT")
-        self.Print()
-        self.Select()
+        #self.Mutate()
+        #self.child.Evaluate("DIRECT")
+        #self.Print()
+        #self.Select()
 
     def Spawn(self):
+        self.children = {}
         #self.child = copy.deepcopy(self.parent)
         for i in range(len(self.parents)):
             self.children[i].Set_ID()
-            self.children = copy.deepcopy(self.parents)
+            self.children[i] = copy.deepcopy(self.parents[i])
             self.nextAvailableID = self.nextAvailableID + 1
 
  
