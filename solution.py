39a23ebae4630aedaae36cadad9a202afe70bbdd
Kevin Motia <kevinmotia@Kevins-MacBook-Pro.local>
2022-03-07 18:57:10 -0500

    step 37 quadruped

--- solution.py
+++ solution.py
@@ -14,7 +14,7 @@ height = 1
 # Torso_FrontLeg joint has no upstream joint because we want Torso to be the parent link again. So we use abs coords for Torso_FrontLeg.
 x = -3
 y = 3
-z = .5
+z = 0.5
 
 #Torso position
 x0 = 0 #x0 = 1.5 
@@ -23,21 +23,30 @@ z0 = 1 #z0 = 1.5
 
 #BackLeg Position
 x1 = 0
-y1 = -.5
+y1 = -0.5
 z1 = 0
 #BackLeg Size (Dimensions)
-l1 = .2
+l1 = 0.2
 w1 = 1
-h1 = .2
+h1 = 0.2
 
 #FrontLeg Position
 x2 = 0
-y2 = .5
+y2 = 0.5
 z2 = 0
 #FrontLeg Size (Dimensions)
-l2 = .2
+l2 = 0.2
 w2 = 1
-h2 = .2
+h2 = 0.2
+
+#LeftLeg Position
+x3 = -0.5
+y3 = 0
+z3 = 0
+#LeftLeg Size (Dimensions)
+l3 = 1
+w3 = 0.2
+h3 = 0.2
 #--------------------------------------------
 
 class SOLUTION:
@@ -58,12 +67,24 @@ class SOLUTION:
 
     def Generate_Body(self): 
         pyrosim.Start_URDF("body.urdf")
+        
+        #Torso
         pyrosim.Send_Cube(name="Torso", pos=[x0,y0,z0] , size=[length,width,height])
-        pyrosim.Send_Joint( name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [0,-.5,1], jointAxis = "1 0 0")
+        
+        #Back Leg
+        pyrosim.Send_Joint( name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [0,-0.5,1], jointAxis = "1 0 0")
         pyrosim.Send_Cube(name="BackLeg", pos=[x1,y1,z1] , size=[l1,w1,h1])
 
-        pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [0,.5,1], jointAxis = "1 0 0")
+        #Frong Leg
+        pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [0,0.5,1], jointAxis = "1 0 0")
         pyrosim.Send_Cube(name="FrontLeg", pos=[x2,y2,z2] , size=[l2,w2,h2])
+
+        #Left Leg
+        pyrosim.Send_Joint( name = "Torso_LeftLeg" , parent= "Torso" , child = "LeftLeg" , type = "revolute", position = [-0.5,0,1], jointAxis = "1 0 0")
+        pyrosim.Send_Cube(name="LeftLeg", pos=[x3,y3,z3] , size=[l3,w3,h3])        
+
+
+
 
         pyrosim.End()
         #exit() # uncommenting this allows you to see effects of code on body.urdf
