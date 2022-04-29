5f4a9eb29f1b7a5564c394105b94b81939eec44a
Kevin Motia <kevinmotia@Kevins-MacBook-Pro.local>
2022-04-28 22:09:11 -0400

    Gathering FINAL data1

---
+++ wrapper.py
@@ -0,0 +1,34 @@
+import os
+
+os.system("python3 search.py")
+print("HELLO DATA1")
+
+os.system("python3 search.py")
+print("HELLO DATA2")
+
+os.system("python3 search.py")
+print("HELLO DATA3")
+
+os.system("python3 search.py")
+print("HELLO DATA4")
+
+os.system("python3 search.py")
+print("HELLO DATA5")
+
+os.system("python3 search.py")
+print("HELLO DATA6")
+
+os.system("python3 search.py")
+print("HELLO DATA7")
+
+
+
+
+
+
+
+
+
+
+
+
