85d1318b3c989bbc9e042ec134ca02389e98a7f6
Kevin Motia <kevinmotia@Kevins-MacBook-Pro.local>
2022-04-29 13:52:20 -0400

    data collection for FINAL triped2

---
+++ wrapper.py
@@ -0,0 +1,202 @@
+import os
+import time
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA1")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA2")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA3")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA4")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA5")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA6")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA7")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA8")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA9")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA10")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA11")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA12")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA13")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA14")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA15")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA16")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA17")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA18")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA19")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA20")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA21")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA22")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA23")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA24")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA25")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA26")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA27")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA28")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA29")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA30")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA31")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA32")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA33")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA34")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA35")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA36")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA37")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA38")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA39")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA40")
+time.sleep(0.1)
