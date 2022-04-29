19f920ff58e6dcb06a214aab1a7166530c951783
Kevin Motia <kevinmotia@Kevins-MacBook-Pro.local>
2022-04-29 16:07:38 -0400

    wrapper loop

--- wrapper.py
+++ wrapper.py
@@ -1,75 +1,15 @@
 import os
 import time
 
-os.system("python3 search.py")
-time.sleep(0.1)
-print("HELLO DATA1")
-time.sleep(0.1)
 
-os.system("python3 search.py")
-time.sleep(0.1)
-print("HELLO DATA2")
-time.sleep(0.1)
+zzz = 1
 
-os.system("python3 search.py")
-time.sleep(0.1)
-print("HELLO DATA3")
-time.sleep(0.1)
+for i in range(100):
+    os.system("python3 search.py")
+    time.sleep(zzz)
+    print("HELLO DATA",i)
+    time.sleep(zzz)
 
-os.system("python3 search.py")
-time.sleep(0.1)
-print("HELLO DATA4")
-time.sleep(0.1)
-
-os.system("python3 search.py")
-time.sleep(0.1)
-print("HELLO DATA5")
-time.sleep(0.1)
-
-os.system("python3 search.py")
-time.sleep(0.1)
-print("HELLO DATA6")
-time.sleep(0.1)
-
-os.system("python3 search.py")
-time.sleep(0.1)
-print("HELLO DATA7")
-time.sleep(0.1)
-
-os.system("python3 search.py")
-time.sleep(0.1)
-print("HELLO DATA8")
-time.sleep(0.1)
-
-os.system("python3 search.py")
-time.sleep(0.1)
-print("HELLO DATA9")
-time.sleep(0.1)
-
-os.system("python3 search.py")
-time.sleep(0.1)
-print("HELLO DATA10")
-time.sleep(0.1)
-
-os.system("python3 search.py")
-time.sleep(0.1)
-print("HELLO DATA11")
-time.sleep(0.1)
-
-os.system("python3 search.py")
-time.sleep(0.1)
-print("HELLO DATA12")
-time.sleep(0.1)
-
-os.system("python3 search.py")
-time.sleep(0.1)
-print("HELLO DATA13")
-time.sleep(0.1)
-
-os.system("python3 search.py")
-time.sleep(0.1)
-print("HELLO DATA14")
-time.sleep(0.1)
 
 
 
