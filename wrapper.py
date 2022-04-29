eeb44fcd160bd9e502edac9c7ff10e1268bad9ae
Kevin Motia <kevinmotia@Kevins-MacBook-Pro.local>
2022-04-28 23:02:17 -0400

    Data collection FINAL 2

--- wrapper.py
+++ wrapper.py
@@ -1,25 +1,75 @@
 import os
+import time
 
 os.system("python3 search.py")
+time.sleep(0.1)
 print("HELLO DATA1")
+time.sleep(0.1)
 
 os.system("python3 search.py")
+time.sleep(0.1)
 print("HELLO DATA2")
+time.sleep(0.1)
 
 os.system("python3 search.py")
+time.sleep(0.1)
 print("HELLO DATA3")
+time.sleep(0.1)
 
 os.system("python3 search.py")
+time.sleep(0.1)
 print("HELLO DATA4")
+time.sleep(0.1)
 
 os.system("python3 search.py")
+time.sleep(0.1)
 print("HELLO DATA5")
+time.sleep(0.1)
 
 os.system("python3 search.py")
+time.sleep(0.1)
 print("HELLO DATA6")
+time.sleep(0.1)
 
 os.system("python3 search.py")
+time.sleep(0.1)
 print("HELLO DATA7")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA8")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA9")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA10")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA11")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA12")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA13")
+time.sleep(0.1)
+
+os.system("python3 search.py")
+time.sleep(0.1)
+print("HELLO DATA14")
+time.sleep(0.1)
 
 
 
