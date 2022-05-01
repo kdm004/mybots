eb947034fdfaa7f2c8e8060554ebbb8ab730077c
Kevin Motia <kevinmotia@Kevins-MacBook-Pro.local>
2022-05-01 17:07:48 -0400

    random fileName ready for data collection

--- parallelHillClimber.py
+++ parallelHillClimber.py
@@ -4,6 +4,7 @@ import constants as c
 import copy
 import os
 import numpy
+import random
 #------------------------------------
 class PARALLEL_HILL_CLIMBER:
     def __init__(self):
@@ -78,5 +79,5 @@ class PARALLEL_HILL_CLIMBER:
 
     def Results(self):
         numpy.savetxt('resultsPentapod2.out', self.record, delimiter=',', fmt='%.4f') #LOOK
-        numpy.save('resultsPentapod2'+'.npy', self.record) #LOOK
-        numpy.savetxt('resultsPentapod2.csv', self.record, delimiter=',', fmt='%.4f') #LOOK
+        #numpy.save('resultsPentapod2'+'.npy', self.record) #LOOK
+        numpy.savetxt('resultsPentapod2_'+str(random.randint(3,99999))+'.csv', self.record, delimiter=',', fmt='%.4f') #LOOK
