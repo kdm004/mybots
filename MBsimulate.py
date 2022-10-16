aa4b3676b389367a0fc09a80919744f70c2efe6e
Kevin Motia <kevindmotia@gmail.com>
2022-10-16 12:15:08 -0400

    Added more world blocks to environ

--- MBsimulate.py
+++ MBsimulate.py
@@ -39,7 +39,9 @@ import os
 
 #------------------------------------
 pyrosim.Start_SDF("obstacleWorld.sdf")
-pyrosim.Send_Cube(name="Box", pos=[-3,3,0.5] , size=[1,1,1]) 
+for x in range(-5, -15-5,-5):
+        for y in range(0, 15, 5):
+            pyrosim.Send_Cube(name="Box", pos=[x,y,.5] , size=[1/3,1/3,1/3]) 
 pyrosim.End()
 
 manyBots_simulation = MB_SIMULATION()
