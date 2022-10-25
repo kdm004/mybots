154fb765802c1045e28dcf9c8730c3ccf0bb1e18
Kevin Motia <kevindmotia@gmail.com>
2022-10-24 21:15:07 -0400

    Corrected Get_Obstacle_Fitness() in ROBOT. Data collection attempt2

--- robot.py
+++ robot.py
@@ -84,6 +84,7 @@ class ROBOT:
         # Write fitness to txt file
         f = open('obstacleEnv_fitnesses.txt','a')
         f.write(str(xCoordinateOfLinkZero))
+        f.write('\n')
         f.close
 
  # This function is to return the fitness of an obstacle sim fitness. The func for empty env sim fitness is in parallelHC under show_best. 
