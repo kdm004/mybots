073a84b39be9b2639f80a9b744a8101ebe185d42
Kevin Motia <kevindmotia@gmail.com>
2022-10-29 23:41:52 -0400

    15x15 obstacleWorld

--- MBsimulate.py
+++ MBsimulate.py
@@ -13,8 +13,8 @@ import os
 
 
 pyrosim.Start_SDF("obstacleWorld.sdf")
-for x in range(-5, -15,-2):
-        for y in range(-4, 10, 2):
+for x in range(-15, -17,-2):          #-5, -15, -2
+        for y in range(-15, 17, 2):   # -4, 10, 2
             pyrosim.Send_Cube(name="Box", pos=[x,y,.5] , size=[1/3,1/3,1/3]) 
 pyrosim.End()
 
