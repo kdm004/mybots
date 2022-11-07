0551aa4f55eca298be81b1d1a567ed1df764e336
Kevin Motia <kevindmotia@gmail.com>
2022-11-06 20:58:31 -0500

    Uncommented obstacles

--- MBsimulate.py
+++ MBsimulate.py
@@ -13,9 +13,9 @@ import os
 
 
 pyrosim.Start_SDF("obstacleWorld.sdf")
-# for x in range(12, -34-2,-2): # -4
-#        for y in range(-60, 60+2, 2): #-4
-#            pyrosim.Send_Cube(name="Box", pos=[x,y,.5] , size=[1/3,1/3,1/3]) 
+for x in range(12, -34-2,-2): # -4
+       for y in range(-60, 60+2, 2): #-4
+           pyrosim.Send_Cube(name="Box", pos=[x,y,.5] , size=[1/3,1/3,1/3]) 
 pyrosim.End()
 #---------------------------------------------------------------------------------------------------------------------
 
