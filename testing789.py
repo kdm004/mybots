0d002f9322bc143be4f2fbd298f54855748ae753
Kevin Motia <kevindmotia@gmail.com>
2023-01-05 15:09:14 -0500

    how to save and load matrices

--- testing789.py
+++ testing789.py
@@ -2,21 +2,28 @@
 import numpy as np
 import random
 import constants as c
-
+import sys
 # weights = np.random.rand(c.numSensorNeurons+1,c.numMotorNeurons)    # +1 row for the extra vector of leg parts                                                  
 # weights = weights * 2 - 1   
 # weights[9] = 1                                      # Assign value of 1 to every cell in last row
 
 # print(weights)
 
+argLength = len(sys.argv)
+print(argLength)
 
+if sys.argv[1] == '-continue':
+    # use weights from file
+    inputMatrix = np.loadtxt("testing789.txt", dtype = 'f' , delimiter=' ')               # This code block will read in the matrix from a file. 
+    print(inputMatrix)
 
-weights = np.random.rand(c.numSensorNeurons+1,c.numMotorNeurons)    # +1 row for the extra vector of leg parts                                                  
+else:
+    weights = np.random.rand(c.numSensorNeurons+1,c.numMotorNeurons)    # +1 row for the extra vector of leg parts                                                  
+    for i in range(7):
+        weights[9][i] = random.uniform(.5,1.5)
+    print(weights)
 
-#weights[0][1] = 1
-#print(weights)
-for i in range(7):
-    weights[9][i] = random.uniform(.5,1.5)
-
-
-print(weights)
+    # write out weights to file
+    someFile = open('testing789.txt', 'w')
+    np.savetxt('testing789.txt',weights)
+    someFile.close()
