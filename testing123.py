55b6e8b1cddccc2bb43c4339954be286f5420213
Kevin Motia <kevindmotia@gmail.com>
2023-01-06 19:43:12 -0500

    pickled list of matrices test file success

--- testing123.py
+++ testing123.py
@@ -1,18 +1,55 @@
+import numpy as np
+import random
+import constants as c
+import sys
+import pickle
 import os
-import glob
-from testing456 import NUMBERS as NUMBERS
 
-myList = [(1,2,3),(4,5,6)]
 
-class DISPLAY:
-    def __init__(self,index):
-        self.index = index
-        self.numbers = [(1,2,3),(4,5,6),(7,8,9)]
 
-        self.mySum = NUMBERS(*self.numbers[index])
 
-    def Print(self):
-        print(self.mySum.Add_Numbers())
+
+
+if sys.argv[1] == '-continue':
+    with open('testing123.txt', 'rb') as pickledFile:
+        loadedMatrixList = pickle.load(pickledFile)
+        print(loadedMatrixList[1])
+
+
+else:
+
+    # if pickledFile doesn't exist, initialize empty weightsList
+    weightsList = []
+
+    # if pickledFile exists, open it and load weightsList
+    if os.path.exists('testing123.txt'):
+        with open("testing123.txt", "rb") as pickledFile:
+            # Load weightsList from pickledFile
+            weightsList = pickle.load(pickledFile)
+
+    # If pickledFile is empty, initialize weightsList 
+    if len(weightsList) == 0:
+        weightsList = []
+
+    # Add a new matrix to weightsList
+    weights = np.random.rand(9+1, 8)                                                  
+    for i in range(7):
+        weights[9][i] = random.uniform(.5,1.5)
+    weightsList.append(weights)
+    print(weights)
+
+    # Overwrite pickledFile with new weightsList
+    with open("testing123.txt", "wb") as pickledFile:       
+        pickle.dump(weightsList, pickledFile) 
+
+
+
+
+
+
+
+
+
 
 
 
