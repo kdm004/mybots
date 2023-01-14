9984281770d38f1642d029dd130d75be5571d7ed
Kevin Motia <kevindmotia@gmail.com>
2023-01-14 14:28:40 -0500

    forgot search.py in last commit

--- search.py
+++ search.py
@@ -1,20 +1,17 @@
 
-
 import os
 import time
 import glob
 import constants as c
 from parallelHillClimber import PARALLEL_HILL_CLIMBER
 import nonChampDeleter as NDC
-import sys
 #------------------------- 
 
-commandLineArg = sys.argv # sys.argv = ['search.py', 'continue']         
 
 for botIndex in range(10):
     NDC.NonChampDeleter()
 
-    phc1 = PARALLEL_HILL_CLIMBER(botIndex, commandLineArg[1], commandLineArg[2]) # commandLineArg[0] = -c, commandLineArg[2] = emptySwarmIndex
+    phc1 = PARALLEL_HILL_CLIMBER(botIndex)
     time.sleep(1)
     phc1.Evolve()
     time.sleep(1)
@@ -45,29 +42,3 @@ for botIndex in range(10):
 
 
 
-
-
-
-
-#phc2 = PARALLEL_HILL_CLIMBER()
-#phc2.Evolve()
-#phc2.Show_Best()
-
-#phc3 = PARALLEL_HILL_CLIMBER()
-#phc3.Evolve()
-#phc3.Show_Best()
-
-
-
-#--------------------------
-
-#for i in range(2): #this is number of simulation windows that will pop up. 3 are currently popping
-    # up because there's another "simulate.py" call in SOLUTION's Evaluate(self)
-#    os.system("python3 generate.py")
-#    os.system("python3 simulate.py")
-
-
-
-
-# Initially, we had 1 instance called phc. Now, we have phc1, phc2, and phc3
-# So now, we should only need to run search.py once to get brain0,brain1,brain2 | brain3, brain4, brain5 | brain6, brain7, brain8.
