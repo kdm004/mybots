5092b20d10261fa6850144d923d46fccfd230c33
Kevin Motia <kevindmotia@gmail.com>
2023-10-01 23:55:12 -0400

    evolved 5x100 case2

--- MBsimulate.py
+++ MBsimulate.py
@@ -8,39 +8,72 @@ import time
 from MBsimulation import MANYBOTS_SIMULATION as MB_SIMULATION
 import sys
 import os
+import math
 
 
 
+# Your list of positions
+positions = [
+    (0, -18),
+    (0, -14),
+    (0, -10),
+    (0, -6),
+    (0, -2),
+    (0, 2),
+    (0, 6),
+    (0, 10),
+    (0, 14),
+    (0, 18)
+]
 
+# Function to calculate the Euclidean distance between two points (coordinates)
+def distance(point1, point2):
+    x1, y1 = point1
+    x2, y2 = point2
+    return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+
+# Your code to determine currentBot
+def Get_Current_Bot_Number():
+    currentBot = 0
+    currentSwarm = 0
+    if os.path.exists('obstacleEnv_fitnesses.txt'):
+        fp = open('obstacleEnv_fitnesses.txt', 'r') 
+        lines = fp.readlines()
+        cleanLines = []
+        for entry in lines:
+            cleanLines.append(entry.replace('\n',''))
+        cleanLines = list(map(float, cleanLines))
+        fp.close()
+        
+        currentBot = len(cleanLines) % 10
+        currentSwarm = int((len(cleanLines)-1 - currentBot) / 10)
+
+    return currentBot, currentSwarm
+
+# Define the number of closest coordinates to skip
+num_coordinates_to_skip = 5
+
+currentBot, currentSwarm = Get_Current_Bot_Number()
+
+# Your Pyrosim SDF setup code here
 pyrosim.Start_SDF("obstacleWorld.sdf")
-for x in range(10, -22-2,-2): # -4
-       for y in range(-28, 28+2, 2): #-4
-           pyrosim.Send_Cube(name="Box", pos=[x,y,.5] , size=[1/3,1/3,1/3]) 
+for x in range(10, -22-2, -2): 
+    for y in range(-28, 28+2, 2): 
+        current_coordinate = (x, y)
+        current_position = positions[currentBot]
+        distances = [(distance(current_position, (x, y)), (x, y)) for x in range(10, -22-2, -2) for y in range(-28, 28+2, 2)]
+        distances.sort(key=lambda x: x[0])
+        coordinates_to_skip = [coord for _, coord in distances[:num_coordinates_to_skip]]
+        if current_coordinate in coordinates_to_skip:
+            continue  # Skip this coordinate
+        pyrosim.Send_Cube(name="Box", pos=[x, y, .5], size=[1/3, 1/3, 1/3]) 
+
 pyrosim.End()
-#---------------------------------------------------------------------------------------------------------------------
-
-
-# MBSIM = MB_SIMULATION(1)
-# while not os.path.exists("obstacleWorld.sdf"):
-#         time.sleep(0.01)
-# MBSIM.Run()
-# #MBSIM.Shift_Lines() We comment this out for case1 because if the top line is deleted by Shift_Lines(), that would change the index of the overall champion of the 35 controllers
-# MBSIM.Get_Fitness()
 
 
 
-
-
-# MBSIM2 = MB_SIMULATION(2)
-# while not os.path.exists("obstacleWorld.sdf"):
-#         time.sleep(0.01)
-# MBSIM2.Run()
-# #MBSIM.Shift_Lines() We comment this out for case1 because if the top line is deleted by Shift_Lines(), that would change the index of the overall champion of the 35 controllers
-# MBSIM2.Get_Fitness()
-
-for outerLoopIndex in range(70): # 34 because we want the final line number to be 350, which will be swarmIndex + 10
-        for innerLoopIndex in range(10):
-                MBSIM = MB_SIMULATION(innerLoopIndex, outerLoopIndex)
-                MBSIM.Run()
-                MBSIM.Get_Fitness()
-
+for swarmIndex in range(int(700/10)):
+    for botIndex in range(currentBot, 10):
+        MBSIM = MB_SIMULATION(botIndex, swarmIndex)
+        MBSIM.Run()
+        MBSIM.Get_Fitness()
