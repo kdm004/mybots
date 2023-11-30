96f877dd4b6f20665b8d62c2278df5034e91cce6
Kevin Motia <kevindmotia@gmail.com>
2023-11-30 14:06:30 -0500

    refactored solution.py a bit

--- solution.py
+++ solution.py
@@ -6,57 +6,7 @@ import time
 import constants as c
 import numpy as np
 #--------------------------------------------
-#Cube size (length, width, height) and position (x,y,z)
-length = 1
-width = 1
-height = 1
 
-#we want the world block to be at (-3, 3, .5)
-# Torso_FrontLeg joint has no upstream joint because we want Torso to be the parent link again. So we use abs coords for Torso_FrontLeg.
-x = -3
-y = 3
-z = 0.5
-
-#Torso position
-x0 = 0 #x0 = 1.5 
-y0 = 0 #y0 = 0
-z0 = 1 #z0 = 1.5
-
-#BackLeg Position
-x1 = 0
-y1 = -0.5
-z1 = 0
-#BackLeg Size (Dimensions)
-l1 = 0.2
-w1 = 1
-h1 = 0.2
-
-#FrontLeg Position
-x2 = 0
-y2 = 0.5
-z2 = 0
-#FrontLeg Size (Dimensions)
-l2 = 0.2
-w2 = 1
-h2 = 0.2
-
-#LeftLeg Position
-x3 = -0.5
-y3 = 0
-z3 = 0
-#LeftLeg Size (Dimensions)
-l3 = 1
-w3 = 0.2
-h3 = 0.2
-
-#RightLeg Position
-x4 = 0.5
-y4 = 0
-z4 = 0
-#RightLeg Size (Dimensions)
-l4 = 1
-w4 = 0.2
-h4 = 0.2
 #--------------------------------------------
 
 class SOLUTION:
@@ -65,10 +15,8 @@ class SOLUTION:
         self.overallBot = int(overallBot)
         self.continueOrNone = continueOrNone
         self.populationID = int(populationID) # obtained from parallelHillClimber constructor
-        
         self.weights = numpy.random.rand(c.numSensorNeurons,c.numMotorNeurons)
         self.weights = self.weights * 2 - 1
-
 
         if self.continueOrNone == 'continue': 
             with open('weightsFiles/weights' + str(self.overallBot) + '_' + str(self.populationID) + '.txt', 'r') as pickleFile: # use botNumber
@@ -80,6 +28,7 @@ class SOLUTION:
             self.weights = np.random.rand(c.numSensorNeurons+1,c.numMotorNeurons)   
             self.weights = self.weights * 2 - 1    
 
+
     def Evaluate(self,directOrGUI):
         pass
 
@@ -89,89 +38,73 @@ class SOLUTION:
         #pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height]) 
         pyrosim.End()
 
+
     def Generate_Body(self,xi,yi): 
-
+# Start generating robot body and name the body file
         pyrosim.Start_URDF("bodyFiles/body"+str(xi)+str(yi)+".urdf")
-        
-        #Torso
-        pyrosim.Send_Cube(name="Torso", pos=[0+xi,0+yi,1] , size=[length,width,height])
-            
-    # Upper Extremities
-        #Back Leg
+#Torso
+        pyrosim.Send_Cube(name="Torso", pos=[0+xi,0+yi,1] , size=[1,1,1])
+# Upper Extremities
+    #Back Leg
         pyrosim.Send_Joint( name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [0+xi,-0.5+yi,1], jointAxis = "1 0 0")
-        pyrosim.Send_Cube(name="BackLeg", pos=[0,-0.5,0] , size=[l1,w1,h1])
-
-        #Front Leg
+        pyrosim.Send_Cube(name="BackLeg", pos=[0,-0.5,0] , size=[0.2, 1, 0.2])
+    #Front Leg
         pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [0+xi,0.5+yi,1], jointAxis = "1 0 0")
-        pyrosim.Send_Cube(name="FrontLeg", pos=[0,0.5,0] , size=[l2,w2,h2])
-
-            #Left Leg
+        pyrosim.Send_Cube(name="FrontLeg", pos=[0,0.5,0] , size=[0.2, 1, 0.2])
+    #Left Leg
         pyrosim.Send_Joint( name = "Torso_LeftLeg" , parent= "Torso" , child = "LeftLeg" , type = "revolute", position = [-0.5+xi,0+yi,1], jointAxis = "0 1 0")
-        pyrosim.Send_Cube(name="LeftLeg", pos=[-0.5,0,0] , size=[l3,w3,h3])        
-
-            #Right Leg
+        pyrosim.Send_Cube(name="LeftLeg", pos=[-0.5,0,0] , size=[1, 0.2, 0.2])       
+    #Right Leg
         pyrosim.Send_Joint( name = "Torso_RightLeg" , parent= "Torso" , child = "RightLeg" , type = "revolute", position = [0.5+xi,0+yi,1], jointAxis = "0 1 0")
-        pyrosim.Send_Cube(name="RightLeg", pos=[0.5,0,0] , size=[l4,w4,h4])   
-
-    # Lower Extremities
-        #Back Lower Leg
+        pyrosim.Send_Cube(name="RightLeg", pos=[0.5,0,0] , size=[1, 0.2, 0.2])   
+# Lower Extremities
+    #Back Lower Leg
         pyrosim.Send_Joint( name = "BackLeg_BackLowerLeg" , parent= "BackLeg", child = "BackLowerLeg", type = "revolute", position = [0, -1, 0], jointAxis = "1 0 0")
-        pyrosim.Send_Cube(name="BackLowerLeg", pos=[0, 0, -0.5] , size=[0.2, 0.2, 1])
-            
-        #Front Lower Leg
+        pyrosim.Send_Cube(name="BackLowerLeg", pos=[0, 0, -0.5] , size=[0.2, 0.2, 1])   
+    #Front Lower Leg
         pyrosim.Send_Joint( name = "FrontLeg_FrontLowerLeg" , parent= "FrontLeg", child = "FrontLowerLeg", type = "revolute", position = [0, 1, 0], jointAxis = "1 0 0")
         pyrosim.Send_Cube(name="FrontLowerLeg", pos=[0, 0, -0.5] , size=[0.2, 0.2, 1])
-
-        #Left Lower Leg
+    #Left Lower Leg
         pyrosim.Send_Joint( name = "LeftLeg_LeftLowerLeg" , parent= "LeftLeg", child = "LeftLowerLeg", type = "revolute", position = [-1, 0, 0], jointAxis = "0 1 0")
         pyrosim.Send_Cube(name="LeftLowerLeg", pos=[0, 0, -0.5] , size=[0.2, 0.2, 1])
-
-        #Right Lower Leg
+    #Right Lower Leg
         pyrosim.Send_Joint( name = "RightLeg_RightLowerLeg", parent= "RightLeg", child = "RightLowerLeg", type = "revolute", position = [1, 0, 0], jointAxis = "0 1 0")
         pyrosim.Send_Cube(name="RightLowerLeg", pos=[0, 0, -0.5] , size=[0.2, 0.2, 1])
-
         pyrosim.End()
         #exit() # uncommenting this allows you to see effects of code on body.urdf
 
-    def Generate_Brain(self):  #ADDED TO ROBOT_BRAIN
 
+    def Generate_Brain(self):
+# Start neural network. Give 
         pyrosim.Start_NeuralNetwork("brainFiles/brain" + str(self.myID) + ".nndf") #changed from brain.nndf
-
-
 # Upper Extremity Sensor Neurons
         pyrosim.Send_Sensor_Neuron(name = 0 , linkName = "Torso")
         pyrosim.Send_Sensor_Neuron(name = 1 , linkName = "BackLeg")
         pyrosim.Send_Sensor_Neuron(name = 2 , linkName = "FrontLeg")
         pyrosim.Send_Sensor_Neuron(name = 3 , linkName = "LeftLeg")
         pyrosim.Send_Sensor_Neuron(name = 4 , linkName = "RightLeg")
-
 # Lower Extremity Sensor Neurons
         pyrosim.Send_Sensor_Neuron(name = 5 , linkName = "BackLowerLeg")
         pyrosim.Send_Sensor_Neuron(name = 6 , linkName = "FrontLowerLeg")
         pyrosim.Send_Sensor_Neuron(name = 7 , linkName = "LeftLowerLeg")
         pyrosim.Send_Sensor_Neuron(name = 8 , linkName = "RightLowerLeg")
-
 # Upper Extremity Motor Neurons
         pyrosim.Send_Motor_Neuron( name = 9, jointName = "Torso_BackLeg")
         pyrosim.Send_Motor_Neuron( name = 10, jointName = "Torso_FrontLeg")
         pyrosim.Send_Motor_Neuron( name = 11, jointName = "Torso_LeftLeg")
         pyrosim.Send_Motor_Neuron( name = 12, jointName = "Torso_RightLeg")
-
 # Lower Extremity Motor Neurons
         pyrosim.Send_Motor_Neuron( name = 13, jointName = "BackLeg_BackLowerLeg")
         pyrosim.Send_Motor_Neuron( name = 14, jointName = "FrontLeg_FrontLowerLeg")
         pyrosim.Send_Motor_Neuron( name = 15, jointName = "LeftLeg_LeftLowerLeg")
         pyrosim.Send_Motor_Neuron( name = 16, jointName = "RightLeg_RightLowerLeg")
-
-
-   
         for currentRow in range(c.numSensorNeurons):
             for currentColumn in range(c.numMotorNeurons):
-                pyrosim.Send_Synapse( sourceNeuronName = currentRow , targetNeuronName = currentColumn+c.numSensorNeurons , weight = self.weights[currentRow][currentColumn] ) #step 7 randomsearch
-
+                pyrosim.Send_Synapse( sourceNeuronName = currentRow , targetNeuronName = currentColumn+c.numSensorNeurons , weight = self.weights[currentRow][currentColumn] )
         pyrosim.End()
 
-    def Mutate(self): #ADDED TO ROBOT_BRAIN
+
+    def Mutate(self): 
         randomRow = random.randint(0,c.numSensorNeurons - 1) #(0,2) represents 0th, 1st, and 2nd rows
         randomColumn = random.randint(0,c.numMotorNeurons - 1) #(0,1) represents 0th and 1st column
         self.weights[randomRow, randomColumn] = random.random() * 2 - 1
@@ -188,39 +121,21 @@ class SOLUTION:
                 pickleFile.close()
 
 
-
-
-    def Set_ID(self): #ADDED TO ROBOT_BRAIN
+    def Set_ID(self):
         self.myID
 
     def Start_Simulation(self, directOrGUI):
         self.Create_World()
-
-#----------------------------------------------------------------------------------------------------
-#        self.body_list = []
-#        for i in range(0, 20, 5):                         # range(0, 15, 5) gets us body0, body5, body10
-#            self.body_list.append(self.Generate_Body(i))
-#        for i in range(3): # this tells us the index of the list. Inside the list, we have self.body_list[1], self.body_list[2], self.body_list[3] which store body0, body5, body10.
-#            self.body_list[i]
-#----------------------------------------------------------------------------------------------------
-
-
-        self.Generate_Brain() #ADDED TO ROBOT_BRAIN
+        self.Generate_Brain() 
         os.system("python3 simulate.py " + directOrGUI + " " + str(self.myID)+ ' ' + str(self.overallBot) + ' ' + str(self.continueOrNone) + ' ' + str(self.populationID) +" &") # changed from "DIRECT" to directOrGUI... added " &"
-
-    def Wait_For_Simulation_To_End(self):
-        while not os.path.exists("fitness"+ str(self.myID) + ".txt"):
+    
+    def wait_for_simulation_to_end(self):
+        fitness_file_path = f"fitness{self.myID}.txt"
+        while not os.path.exists(fitness_file_path):
             time.sleep(0.01)
-
-        fitnessFile = open("fitness"+ str(self.myID) + ".txt","r")
-        time.sleep(0.1)
-        lines = fitnessFile.read()
-        time.sleep(0.1)
-        self.fitness = float(lines)
-        #self.fitness = float(fitnessFile.read()) #Used fitnessFile, they normally use f
-        #print("fitness"+str(self.myID)+"=", self.fitness) # commented out for step 75 parallelHC
-        fitnessFile.close()
-        os.system("rm fitness"+ str(self.myID) + ".txt")
-        while os.path.exists("fitness"+str(self.myID)+".txt"):
-            os.system("rm fitness"+ str(self.myID) + ".txt")
-   
+        with open(fitness_file_path, 'r') as fitness_file:
+            self.fitness = float(fitness_file.read())
+        os.remove(fitness_file_path)
+        while os.path.exists(fitness_file_path):
+            os.remove(fitness_file_path)
+            time.sleep(0.01)
