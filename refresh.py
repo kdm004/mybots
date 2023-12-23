55f6acae6bbba2871574f40bcddce05fc6a0ec18
Kevin Motia <kevindmotia@gmail.com>
2023-12-22 22:40:55 -0500

    update refresh.py

--- refresh.py
+++ refresh.py
@@ -3,4 +3,5 @@ import os
 os.system("rm bodies/body*.urdf")
 os.system("rm brains/brain*.nndf")
 os.system("rm data/*.npy")
-os.system("fitnessCurves*.txt")
+os.system("rm fitnessCurves/fitnessCurve*.txt")
+os.system("rm fitness*.txt")
